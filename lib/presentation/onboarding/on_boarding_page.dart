797b240e4e031cd8daeb15537dcad3c61c5d60f5
Inesa <inesaghalumyan@gmail.com>
2022-02-17 19:03:46 +0400

    on boarding screen: pages and bottom sheet

---
+++ lib/presentation/onboarding/on_boarding_page.dart
@@ -0,0 +1,38 @@
+import 'package:architeture_project/presentation/onboarding/slider_object.dart';
+import 'package:architeture_project/presentation/resources/values_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  SliderObject sliderObject;
+
+  OnBoardingPage(this.sliderObject, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const SizedBox(height: AppSize.s40),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            sliderObject.title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            sliderObject.subTitle,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ),
+        const SizedBox(height: AppSize.s60),
+        SvgPicture.asset(sliderObject.image),
+      ],
+    );
+  }
+}
--- lib/presentation/resources/values_manager.dart
+++ lib/presentation/resources/values_manager.dart
@@ -17,6 +17,7 @@ class AppPadding {
 }
 
 class AppSize {
+  static const double s0 = 0;
   static const double s1_5 = 1.5;
   static const double s4 = 4;
   static const double s8 = 8;
@@ -26,6 +27,12 @@ class AppSize {
   static const double s18 = 18;
   static const double s20 = 20;
   static const double s28 = 28;
+  static const double s40 = 40;
   static const double s60 = 60;
   static const double s65 = 65;
+  static const double s100 = 100;
+}
+
+class DurationConstant {
+  static const int d300 = 300;
 }

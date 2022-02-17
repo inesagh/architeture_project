524dc6e91b147873564b08e6e3e908fdfa0f6bac
Inesa <inesaghalumyan@gmail.com>
2022-02-17 12:13:12 +0400

    theme manager: input decoration theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -60,6 +60,33 @@ ThemeData getApplicationTheme() {
       caption: getRegularTextStyle(color: ColorManager.grey1),
       bodyText2: getRegularTextStyle(color: ColorManager.grey),
     ),
+
     //  input decoration theme(text from field)
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(AppPadding.p8),
+      // hint, label, error styles
+      hintStyle: getRegularTextStyle(color: ColorManager.grey1),
+      labelStyle: getMediumTextStyle(color: ColorManager.darkGrey),
+      errorStyle: getRegularTextStyle(color: ColorManager.error),
+      // enabled, focused, error, focused error borders
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide:
+            BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.error, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide:
+            BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+    ),
   );
 }

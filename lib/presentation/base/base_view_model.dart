19db51e0314471fb428c2f8f092240674c84ba29
Inesa <inesaghalumyan@gmail.com>
2022-02-18 10:49:19 +0400

    view model: base view model, inputs & outputs

---
+++ lib/presentation/base/base_view_model.dart
@@ -0,0 +1,12 @@
+abstract class BaseViewModel{
+  // shared var. & func. that will be used through any view model 
+}
+
+abstract class BaseViewModelInputs{
+  void start(); // init the view model 
+  void dispose(); //close the stream
+}
+
+abstract class BaseViewModelOutputs{
+  
+}

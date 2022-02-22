703584e41ce0659f1bf8679f9dfcace77896dea2
Inesa <inesaghalumyan@gmail.com>
2022-02-22 13:49:40 +0400

    data layer: app service client(retrofit) for api

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,13 @@
+import 'package:architeture_project/app/constants.dart';
+import 'package:architeture_project/data/responses/responses.dart';
+import 'package:retrofit/http.dart';
+import 'package:dio/dio.dart';
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String? baseUrl}) = _AppServiceClient;
+
+  @POST("/customer/login")
+  Future<AuthenticationResponse> login();
+}

70c88cc9c2c806a47bf34c4b1e0b61a5bb839c7f
Inesa <inesaghalumyan@gmail.com>
2022-02-22 14:28:20 +0400

    data layer: mappers (responses -> models) with applying null safety

---
+++ lib/data/mapper/mapper.dart
@@ -0,0 +1,28 @@
+//response - (null checking) - model
+
+import 'package:architeture_project/data/responses/responses.dart';
+import 'package:architeture_project/domain/model.dart';
+import 'package:architeture_project/app/extensions.dart';
+
+const EMPTY = "";
+const ZERO = 0;
+
+extension CustomerResponseMapper on CustomerResponse {
+  Customer toDomain() {
+    return Customer(id?.orZero() ?? ZERO, name?.orEmpty() ?? EMPTY,
+        numberOfNotifications?.orZero() ?? ZERO);
+  }
+}
+
+extension ContactsResponseMapper on ContactsResponse {
+  Contacts toDomain() {
+    return Contacts(phone?.orEmpty() ?? EMPTY, link?.orEmpty() ?? EMPTY,
+        email?.orEmpty() ?? EMPTY);
+  }
+}
+
+extension AuthenticationResponseMapper on AuthenticationResponse {
+  Authentication toDomain() {
+    return Authentication(customer?.toDomain(), contacts?.toDomain());
+  }
+}

9b302f8d6deb1d794fc29b7ae4241866104b6d7f
Inesa <inesaghalumyan@gmail.com>
2022-02-22 13:58:03 +0400

    data layer: model corresponding to every response

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -5,3 +5,26 @@ class SliderObject {
 
   SliderObject(this.title, this.subtitle, this.image);
 }
+
+class Customer {
+  int id;
+  String name;
+  int numberOfNotifications;
+
+  Customer(this.id, this.name, this.numberOfNotifications);
+}
+
+class Contacts {
+  String phone;
+  String link;
+  String email;
+
+  Contacts(this.phone, this.link, this.email);
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(this.customer, this.contacts);
+}

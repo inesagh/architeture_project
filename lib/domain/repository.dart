c563e73a074cad2fe4b468986927bb66da94d642
Inesa <inesaghalumyan@gmail.com>
2022-02-22 18:49:54 +0400

    domain layer: repository

---
+++ lib/domain/repository.dart
@@ -0,0 +1,8 @@
+import 'package:architeture_project/data/network/failure.dart';
+import 'package:architeture_project/data/requests/requests.dart';
+import 'package:architeture_project/domain/model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class Repository {
+  Future<Either<Failure, Authentication>> login(LoginRequest loginRequest);
+}

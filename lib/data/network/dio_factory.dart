d3bc11e76e9786b5d613339747fd2d288989c431
Inesa <inesaghalumyan@gmail.com>
2022-02-24 17:31:30 +0400

    data layer: logger interceptor for dio

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -1,5 +1,7 @@
 import 'package:architeture_project/app/constants.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 const String APPLICATION_JSON = "application/json";
 const String CONTECT_TYPE = "content_type";
@@ -23,6 +25,13 @@ class DioFactory {
         connectTimeout: _timeOut,
         receiveTimeout: _timeOut,
         headers: headers);
+
+    if (kReleaseMode) {
+      print("Release mode no logs");
+    } else {
+      dio.interceptors.add(PrettyDioLogger(
+          requestHeader: true, requestBody: true, responseHeader: true));
+    }
 
     return dio;
   }
